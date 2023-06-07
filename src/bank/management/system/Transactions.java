36977ef0a4413cb3fe6aab2253f00cff8e98b0df
MARAMBE00 <pasindurmarambe00@gmail.com>
2023-06-07 22:58:14 +0530

    Creating pin change class

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -82,6 +82,9 @@ public class Transactions extends JFrame implements ActionListener {
         }else if (ae.getSource() == fastCash) {
             setVisible(false);
             new FastCash(pinNumber).setVisible(true);
+        }else if (ae.getSource() == pinChange) {
+            setVisible(false);
+            new PinChange(pinNumber).setVisible(true);
         }
     }
 
